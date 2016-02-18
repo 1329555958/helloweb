cd7cf58dd95bdc740da7e3e2a28b3664f09ff7a6
13295555958@qq.com <wid#228#>
2016-02-18 14:58:59 +0800

    'done'

--- js/app.js
+++ js/app.js
@@ -6,3 +6,15 @@ mui(".mui-bar").on('tap', '.mui-tab-item', function() {
 		id: href
 	})
 });
+mui("body").on("tap", "a", function(e) {
+	var t = this;
+	if (t.href && t.href.indexOf(".html") > 0) {
+		var href = t.getAttribute('href');
+		mui.openWindow({
+			url: href,
+			id: href
+		})
+		mui.preventDefault(e);
+	}
+
+});
